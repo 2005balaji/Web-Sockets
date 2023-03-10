1b9e4b10dba4b5c2bd86e744d7504a85eb03b39a
2005balaji <balajisr000@gmail.com>
2023-03-10 21:00:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    const ws = new WebSocket('ws://localhost:8080');
+
+    ws.onopen = () => {
+      console.log('Connected to server');
+    };
+
+    ws.onmessage = (event) => {
+      console.log('Received message:', event.data);
+      setMessage(event.data);
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>WebSocket Example</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
